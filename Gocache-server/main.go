3dd2b5394195bc5e6ed067fa190271ae91b04eb3
meguriri <1310500986@qq.com>
2023-04-18 22:57:01 +0800

    fix bug

--- Gocache-server/main.go
+++ Gocache-server/main.go
@@ -1,71 +1,14 @@
 package main
 
 import (
-	"bufio"
-	"context"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 
 	"github.com/meguriri/GoCache/server/cache"
 	"github.com/meguriri/GoCache/server/callback"
 	"github.com/meguriri/GoCache/server/config"
 	"github.com/meguriri/GoCache/server/manager"
 )
-
-func CMD(ctx context.Context, m *manager.Manager) {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print(manager.ManagerIP+":"+manager.ManagerPort, ">")
-		res, _, _ := reader.ReadLine()
-		li := strings.Split(string(res), " ")
-		if (li[0] == "set" || li[0] == "SET") && len(li) == 3 {
-			if ok := m.Set(ctx, li[1], []byte(li[2])); !ok {
-				fmt.Println("set error")
-			} else {
-				fmt.Println("OK")
-			}
-		} else if (li[0] == "get" || li[0] == "GET") && len(li) == 2 {
-			res, err := m.Get(ctx, li[1])
-			if err != nil {
-				fmt.Println("(nil)")
-			} else {
-				fmt.Println("\"", string(res), "\"")
-			}
-		} else if (li[0] == "del" || li[0] == "DEL") && len(li) == 2 {
-			res := m.Del(ctx, li[1])
-			if !res {
-				fmt.Println("(integer) 0")
-			} else {
-				fmt.Println("(integer) 1")
-			}
-		} else if li[0] == "exit" {
-			fmt.Println("bye!")
-			return
-		} else if li[0] == "kill" && len(li) == 2 {
-			if ok, err := m.KillPeer(ctx, li[1]); ok {
-				fmt.Println(li[1], "is logout")
-			} else {
-				fmt.Println(li[1], "logout err:", err)
-			}
-		} else if li[0] == "connect" && len(li) == 4 {
-			bytes, _ := strconv.Atoi(li[3])
-			if ok := m.Connect(li[1], li[2], int64(bytes)); ok {
-				fmt.Println(li[2], "is connected")
-			} else {
-				fmt.Println(li[2], "connect err:")
-			}
-		} else if (li[0] == "getall" || li[0] == "GETALL") && len(li) == 2 {
-			res := m.GetAllCache(ctx, li[1])
-			fmt.Println("res:", res)
-		} else if (li[0] == "info" || li[0] == "INFO") && len(li) == 2 {
-			res := m.GetInfo(ctx, li[1])
-			fmt.Println("res:", res)
-		}
-	}
-}
 
 func main() {
 	if err := config.Configinit(); err != nil {
@@ -80,11 +23,7 @@ func main() {
 	}
 
 	manager := manager.NewManager(callback.CallBackFunc(cb))
-	manager.Connect("peer1", "127.0.0.1:8081", 1024*1024)
-	// manager.Connect("peer2", "127.0.0.1:8082", 1024*1024)
-	// manager.Connect("peer3", "127.0.0.1:8083", 1024*1024)
+	//manager.Connect("peer1", "127.0.0.1:8081", 1024*1024)
 
-	ctx := context.Background()
-	CMD(ctx, manager)
-
+	manager.Serve()
 }
--- Gocache-server/manager/manager.go
+++ Gocache-server/manager/manager.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
+	"strconv"
 	"strings"
 	"sync"
 
@@ -41,6 +43,84 @@ func NewManager(callback callback.CallBack) *Manager {
 		localcacheBytes: replacement.MaxBytes,
 		callback:        callback,
 		hash:            *consistenthash.New(DefaultReplicas, nil),
+	}
+}
+
+func (m *Manager) Serve() {
+	listen, err := net.Listen("tcp", m.addr)
+	if err != nil {
+		fmt.Printf("listen err=%v\n", err)
+		return
+	}
+	for {
+		fmt.Println("listen on ", m.addr)
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Printf("Accept() err=%v\n", err)
+		} else {
+			fmt.Printf("client ip=%v\n", conn.RemoteAddr().String())
+		}
+		ctx := context.Background()
+		go m.handler(ctx, conn)
+	}
+}
+
+func (m *Manager) handler(ctx context.Context, conn net.Conn) {
+	defer conn.Close()
+	for {
+		buf := make([]byte, 1024*1024)
+		_, err := conn.Read(buf)
+		req := strings.Trim(string(buf), "\000")
+		if err != nil {
+			fmt.Println("服务器的Read err=", err)
+			return
+		}
+		li := strings.Split(req, " ")
+		resp := []byte{}
+		if (li[0] == "set" || li[0] == "SET") && len(li) == 3 {
+			if ok := m.Set(ctx, li[1], []byte(li[2])); !ok {
+				resp = []byte("set error")
+			} else {
+				resp = []byte("OK")
+			}
+		} else if (li[0] == "get" || li[0] == "GET") && len(li) == 2 {
+			res, err := m.Get(ctx, li[1])
+			if err != nil {
+				resp = []byte("(nil)")
+			} else {
+				resp = []byte("\"" + string(res) + "\"")
+			}
+		} else if (li[0] == "del" || li[0] == "DEL") && len(li) == 2 {
+			res := m.Del(ctx, li[1])
+			if !res {
+				resp = []byte("(integer) 0")
+			} else {
+				resp = []byte("(integer) 1")
+			}
+		} else if li[0] == "exit" {
+			resp = []byte("bye!")
+			return
+		} else if li[0] == "kill" && len(li) == 2 {
+			if ok, err := m.KillPeer(ctx, li[1]); ok {
+				resp = []byte(li[1] + " is logout")
+			} else {
+				resp = []byte(li[1] + "logout err:" + err.Error())
+			}
+		} else if li[0] == "connect" && len(li) == 4 {
+			bytes, _ := strconv.Atoi(li[3])
+			if ok := m.Connect(li[1], li[2], int64(bytes)); ok {
+				resp = []byte(li[2] + " is connected")
+			} else {
+				resp = []byte(li[2] + " connect err:")
+			}
+		} else if (li[0] == "getall" || li[0] == "GETALL") && len(li) == 2 {
+			res := m.GetAllCache(ctx, li[1])
+			fmt.Println("res:", res) //todo
+		} else if (li[0] == "info" || li[0] == "INFO") && len(li) == 2 {
+			res := m.GetInfo(ctx, li[1])
+			fmt.Println("res:", res) //todo
+		}
+		conn.Write(resp)
 	}
 }
 
